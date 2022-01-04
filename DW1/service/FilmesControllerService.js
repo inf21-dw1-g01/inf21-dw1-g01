2e8ba7123a42fe38e992738b3296f5054b5ac508
AnaF <82066444+AnaAzevedo2@users.noreply.github.com>
2022-01-04 17:44:46 +0000

    Update FilmesControllerService.js

--- DW1/service/FilmesControllerService.js
+++ DW1/service/FilmesControllerService.js
@@ -2,26 +2,45 @@
 
 
 /**
- * Delete Filmes
+ * Delete Filmes -------------- alterei !!!!!
  *
  * id Long 
  * no response value expected for this operation
  **/
 exports.deleteFilmes = function(id) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    sql.query("DELETE FROM filmes WHERE id = ?", [id], function (err, res){
+      if (err || !res.affectedRows){
+        console.log(err);
+        console.log(res);
+        reject();
+      }
+      else{
+        console.log(res);
+        resolve({"deleted":id});
+      }
+    });
   });
 }
 
 
 /**
- * Retrieve Filmes
+ * Retrieve Filmes --------------- alterei !!!!!!!!
  *
  * id Long 
  * returns Filmes
  **/
 exports.retrieveFilme = function(id) {
   return new Promise(function(resolve, reject) {
+    sql.query("SELECT * FROM filmes WHERE id = ?", [id], function(err, res){
+      if (err){
+        console.log(err);
+        reject(err);
+      }
+      else{
+        console.log(res);
+        resolve(res[0]);
+      }
     var examples = {};
     examples['application/json'] = {
   "original_title" : "original_title",
@@ -39,16 +58,26 @@ exports.retrieveFilme = function(id) {
       resolve();
     }
   });
+  });
 }
 
 
 /**
- * Retrieve Filmes
+ * Retrieve Filmes ------------- alterei !!!!!
  *
  * returns List
  **/
 exports.retrieveFilmes = function() {
   return new Promise(function(resolve, reject) {
+    sql.query("SELECT * FROM filmes WHERE id = ?", [id], function(err, res){
+      if (err){
+        console.log(err);
+        reject(err);
+      }
+      else{
+        console.log(res);
+        resolve(res[0]);
+      }
     var examples = {};
     examples['application/json'] = [ {
   "original_title" : "original_title",
@@ -75,11 +104,12 @@ exports.retrieveFilmes = function() {
       resolve();
     }
   });
+});
 }
 
 
 /**
- * Update Filmes
+ * Update Filmes ----------------- alterei !!!!!
  *
  * body Filmes 
  * id Long 
@@ -87,7 +117,16 @@ exports.retrieveFilmes = function() {
  **/
 exports.updateFilmes = function(body,id) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    sql.query("UPDATE filmes set id = ?, original_title = ?, release_date = ?, genre = ?, runtime = ?, language = ?, title = ?, adult = ?", [body.id, body.original_title, body.release_date, body.genre, body.runtime, body.language, body.title, body.adult], function (err, res){
+      if(err){
+        console.log(err);
+        reject(err);
+      }
+      else{
+        console.log(res);
+        resolve(id);
+      }
+    });
   });
 }
 
