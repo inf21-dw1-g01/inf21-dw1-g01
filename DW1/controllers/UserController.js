78731c21c6f0f1ea97c4697c95bfe4f47ee53934
AnaF <82066444+AnaAzevedo2@users.noreply.github.com>
2022-01-09 18:22:27 +0000

    Update UserController.js

--- DW1/controllers/UserController.js
+++ DW1/controllers/UserController.js
@@ -3,25 +3,6 @@
 var utils = require('../utils/writer.js');
 var UserController = require('../service/UserControllerService');
 
-module.exports.deleteFavoritos = function deleteFavoritos (req, res, next, fav) {
-  UserController.deleteFavoritos(fav)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.retrieveFavoritos = function retrieveFavoritos (req, res, next, fav) {
-  UserController.retrieveFavoritos(fav)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
 
 module.exports.retrieveUser = function retrieveUser (req, res, next) {
   UserController.retrieveUser()
@@ -35,16 +16,6 @@ module.exports.retrieveUser = function retrieveUser (req, res, next) {
 
 module.exports.retrieveUsers = function retrieveUsers (req, res, next, id, first_name, last_name, mail) {
   UserController.retrieveUsers(id, first_name, last_name, mail)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.updateFavoritos = function updateFavoritos (req, res, next, body, fav) {
-  UserController.updateFavoritos(body, fav)
     .then(function (response) {
       utils.writeJson(res, response);
     })
