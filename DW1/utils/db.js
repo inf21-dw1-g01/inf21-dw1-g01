f8dd889c3302503519eb4b8f082caee964d487ba
unknown <mariofgrodrigues2@gmail.com>
2022-01-09 16:42:31 +0000

    Terceiro Commit

--- DW1/utils/db.js
+++ DW1/utils/db.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '12345678',
+    password: 'mlp1234',
     database: "filmes"
 });
 
